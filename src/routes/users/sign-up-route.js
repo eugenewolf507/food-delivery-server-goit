10a4449c9bb108fde6b1aedc9d359012db8fb02a
Eugene Volkov <eugenewolf507@gmail.com>
2019-08-13 18:22:26 +0300

    add write file

--- src/routes/users/sign-up-route.js
+++ src/routes/users/sign-up-route.js
@@ -1,4 +1,5 @@
-const qs = require("querystring");
+const fs = require("fs");
+const path = require("path");
 
 const saveUser = user => {
   // получить файл с юзером
@@ -16,16 +17,36 @@ const signUpRoute = (request, response) => {
       body = body + data;
 
       console.log("Incoming data!!!!");
+      console.log(body);
+      console.log("Incoming data!!!!");
     });
 
     request.on("end", function() {
-      const post = qs.parse(body);
+      const post = JSON.parse(body);
+      const userFileName = post.username + ".json";
+      const userDbPathName = path.join(
+        __dirname,
+        "../../",
+        "db",
+        "users",
+        userFileName
+      );
+      console.log(userDbPathName);
+      console.log(fs.existsSync(userDbPathName));
       console.log(post);
+      fs.writeFile(userDbPathName, JSON.stringify(post), function(err) {
+        if (err) throw err;
+        console.log("Replaced!");
+      });
     });
   }
 
-  response.writeHead(200);
-  response.end();
+  response.writeHead(200, { "Content-Type": "application/json" });
+  let json = JSON.stringify({
+    status: "success",
+    user: post
+  });
+  response.end(json);
   // Взять username с данных, сохранить в переменную
 
   // Сохраняем данные в <username>.json
