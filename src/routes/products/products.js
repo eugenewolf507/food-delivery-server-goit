c87c0a6610639ccbd7eb318e604c82b605dc7fdf
Eugene Volkov <eugenewolf507@gmail.com>
2019-08-15 20:30:45 +0300

    start to add filtering and routing

--- src/routes/products/products.js
+++ src/routes/products/products.js
@@ -4,12 +4,28 @@ const path = require("path");
 const products = (request, response) => {
   const filePath = path.join(__dirname, "../../", "db", "all-products.json");
 
-  response.writeHead(200, {
-    "Content-Type": "application/json"
+  // response.writeHead(200, {
+  //   "Content-Type": "application/json"
+  // });
+
+  const testId = 19112834;
+  fs.readFile(filePath, function(err, data) {
+    if (err) {
+      return console.error(err);
+    }
+    const initialData = JSON.parse(data);
+    const filteredData = initialData.filter(user => user.id === testId);
+    // console.log("Asynchronous read ==============: " + initialData);
+    response.writeHead(200, { "Content-Type": "application/json" });
+    let json = JSON.stringify({
+      status: "success",
+      products: filteredData
+    });
+    response.end(json);
   });
 
-  const readStream = fs.createReadStream(filePath);
-  readStream.pipe(response);
+  // const readStream = fs.createReadStream(filePath);
+  // readStream.pipe(response);
 };
 
 module.exports = products;
